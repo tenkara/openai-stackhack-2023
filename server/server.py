b50024527aa624a9e6b70700e0beac0b93a9daf2
Josh N <joshnakka1@gmail.com>
2023-03-03 06:27:48 -0500

    Updated requirements.txt

--- server/server.py
+++ server/server.py
@@ -2,12 +2,12 @@ from os import environ as env
 from flask import Flask, jsonify
 from os import environ as env
 from dotenv import load_dotenv, find_dotenv
-from flask_cors import cross_origin
+from flask_cors import CORS, cross_origin
 from jose import jwt
 from flask_sqlalchemy import SQLAlchemy
+from authlib.integrations.flask_oauth2 import ResourceProtector
+from middleware.validator import Auth0JWTBearerTokenValidator
 
-# Create Application
-app = Flask(__name__)
 
 # Load Environment Variables
 ENV_FILE = find_dotenv()
@@ -17,46 +17,56 @@ AUTH0_DOMAIN = env.get("AUTH0_DOMAIN")
 API_IDENTIFIER = env.get("API_IDENTIFIER")
 ALGORITHMS = ["RS256"]
 
+# Create Application
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+
 # Middleware
-from middleware.auth import requires_auth, AuthError
+require_auth = ResourceProtector()
+validator = Auth0JWTBearerTokenValidator(
+    AUTH0_DOMAIN,
+    API_IDENTIFIER
+)
+require_auth.register_token_validator(validator)
 
 # Routes
 
-# Controllers API
+
+@app.route("/")
+def index():
+    return jsonify(message="Hello from SmartHealth!")
+
 @app.route("/public")
-@cross_origin(headers=["Content-Type", "Authorization"])
 def public():
     """No access token required to access this route
     """
     response = "Hello from a public endpoint! You don't need to be authenticated to see this."
     return jsonify(message=response)
 
-
 @app.route("/private")
 @cross_origin(headers=["Content-Type", "Authorization"])
-@cross_origin(headers=["Access-Control-Allow-Origin", "http://localhost:3000"])
-@requires_auth
+@require_auth(None)
 def private():
     """A valid access token is required to access this route
     """
     response = "Hello from a private endpoint! You need to be authenticated to see this."
     return jsonify(message=response)
 
-
-@app.route("/private-scoped")
+@app.route("/chat", methods=["GET"])
 @cross_origin(headers=["Content-Type", "Authorization"])
-@cross_origin(headers=["Access-Control-Allow-Origin", "http://localhost:3000"])
-@requires_auth
-def private_scoped():
-    """A valid access token and an appropriate scope are required to access this route
-    """
-    if requires_scope("read:messages"):
-        response = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
-        return jsonify(message=response)
-    raise AuthError({
-        "code": "Unauthorized",
-        "description": "You don't have access to this resource"
-    }, 403)
+@require_auth(None)
+def chat():
+    return jsonify({
+        "data": [
+            {   
+                "id": 1,
+                "sender": "user",
+                "content": "Hello, how are you?"
+            },
+        ]
+    })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=env.get("PORT", 3010))
