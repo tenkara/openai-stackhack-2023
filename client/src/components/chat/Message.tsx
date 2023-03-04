b0276a243d69a604fb5c20d55e5dac0f9833032e
Josh N <joshnakka1@gmail.com>
2023-03-03 20:33:46 -0500

    Updates

--- client/src/components/chat/Message.tsx
+++ client/src/components/chat/Message.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {
     role: "system" | "user";
     content: string;
+    url?: string;
 };
 
-export default function Message({ role, content }: Props) {
+export default function Message({ role, content, url }: Props) {
     return (
         <div
-            className="mt-4 flex items-center"
+            className="mt-4 flex items-center z-[2]"
             style={{
                 justifyContent: role === "system" ? "flex-start" : "flex-end",
             }}
@@ -16,9 +18,15 @@ export default function Message({ role, content }: Props) {
             <div
                 className={`${
                     role === "system" ? "bg-primary-400" : "bg-secondary-400"
-                } rounded-lg p-4 text-xl text-white max-w-[80%] break-words`}
+                } max-w-[80%] break-words rounded-lg p-4 text-xl text-white`}
             >
                 {content}
+                <br />
+                {url && (
+                    <a className="cursor-pointer text-sm" href={url}>
+                        Here is a link that may help.
+                    </a>
+                )}
             </div>
         </div>
     );
